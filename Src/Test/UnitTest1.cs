17e0f38d15e6a115c48d7e15e62b78615b5351b0
matt-pielat <mateusz.pielat@gmail.com>
2016-04-14 19:27:17 +0200

    Project structure redone.

---
+++ Src/Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KR.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
