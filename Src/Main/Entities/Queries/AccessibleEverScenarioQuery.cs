c818e70c1847cd36650741eec956ef3fe5a5bdc9
siryj <siryj@i.ua>
2016-05-08 13:29:01 +0200

    AccessibleEverScenarioQuery and AccessibleTypicallyScenarioQuery

---
+++ Src/Main/Entities/Queries/AccessibleEverScenarioQuery.cs
@@ -0,0 +1,22 @@
+﻿using KR.Main.Engine;
+using KR.Main.Entities.Conditions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KR.Main.Entities.Queries
+{
+    public class AccessibleEverScenarioQuery :Query
+    {
+        public AccessibleEverScenarioQuery(ICondition pi, ICondition gamma, Scenario scenario)
+        {
+
+        }
+        public override bool Evaluate(World world)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
