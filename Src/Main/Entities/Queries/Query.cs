8dd46db22ef2b451593842646c459c1cfe44baf0
Piotr Izert <piotrizert@gmail.com>
2016-05-09 21:38:57 +0200

    Scenario Executable queries implementation

--- Src/Main/Entities/Queries/Query.cs
+++ Src/Main/Entities/Queries/Query.cs
@@ -1,9 +1,19 @@
 ﻿using KR.Main.Engine;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace KR.Main.Entities
 {
     public abstract class Query
     {
         public abstract bool Evaluate(World world);
+
+        /// <summary>
+        /// Returns collection of edges from given state for a scenario step.
+        /// </summary>
+        protected IEnumerable<Edge> EdgesForStep(World world, State state, ScenarioStep step)
+        {
+            return world.GetEdges(state).Where(egde => egde.Actor.Equals(step.Actor)).Where(edge => edge.Action.Equals(step.Action));
+        }
     }
 }
--- Src/Main/Entities/Queries/ScenarioAlwaysExecutableQuery.cs
+++ Src/Main/Entities/Queries/ScenarioAlwaysExecutableQuery.cs
@@ -9,15 +9,34 @@ namespace KR.Main.Entities.Queries
 {
     public class ScenarioAlwaysExecutableQuery : ScenarioExecutableQuery
     {
-        public ScenarioAlwaysExecutableQuery(Scenario scenario) : base(scenario)
-        {
-
-        }
+        public ScenarioAlwaysExecutableQuery(Scenario scenario) : base(scenario) { }
 
         public override bool Evaluate(World world)
         {
-            // TODO: implement this
-            throw new NotImplementedException();
+            ISet<State> possibleStates = world.GetStates();
+
+            foreach (ScenarioStep step in Scenario.Steps)
+            {
+                ISet<State> newPossibleStates = new HashSet<State>();
+                foreach (State state in possibleStates)
+                {
+                    IEnumerable<Edge> edgesForStep = EdgesForStep(world, state, step);
+                    if (edgesForStep.Count() == 0)
+                    {
+                        // scenario is not executable - no possible edges
+                        return false;
+                    }
+                    // add new possible result states
+                    foreach (Edge edge in edgesForStep)
+                    {
+                        newPossibleStates.Add(edge.To);
+                    }
+                }
+                // replace possible state sets - next iteration
+                possibleStates = newPossibleStates;
+            }
+            // all steps executed - scenario is executable
+            return true;
         }
     }
 }
