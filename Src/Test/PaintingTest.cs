87370597f18bf375b6f95dcea2abf07c19d28ff6
Piotr Waszkiewicz <Waszka23@gmail.com>
2016-06-13 22:28:45 +0200

    Added new test

---
+++ Src/Test/PaintingTest.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KR.Main.Engine;
+using KR.Main.Entities;
+using KR.Main.Entities.Conditions;
+using KR.Main.Entities.Queries;
+using KR.Main.Entities.Statements;
+using System.Collections.Generic;
+
+namespace KR.Test
+{
+    /// <summary>
+    /// Simple test for checking if detecting different types of actions works.
+    /// </summary>
+    [TestClass]
+    public class PaintingTest
+    {
+        protected World world;
+        protected Fluent painted;
+        protected Actor Fred;
+        protected Actor Bill;
+        protected Action clear;
+        protected Action paint;
+
+        protected World CreateITWorld()
+        {
+            var world = World.Instance;
+            painted = new Fluent("painted");
+            paint = new Action("PAINT");
+            clear = new Action("CLEAR");
+            Fred = new Actor("Fred");
+            Bill = new Actor("Bill");
+
+            world.SetActions(new List<Action>() { paint, clear });
+            world.SetActors(new List<Actor>() { Fred, Bill });
+            world.SetFluents(new List<Fluent> { painted });
+
+            var domain = new Domain();
+            domain.AddInitiallyClause(new Initially(new Negation(painted)));
+            domain.AddCausesClause(new Causes(clear, false, null, new Negation(painted)));
+            domain.AddTypicallyCausesClause(new TypicallyCauses(paint, false, new List<Actor>() { Fred }, painted));
+            world.SetDomain(domain);
+            world.Build();
+            return world;
+        }
+
+        [TestMethod]
+        public void TypicallyAccessibleQuery()
+        {
+            world = CreateITWorld();
+            var q = new AccessibleTypicallyQuery(new Negation(painted), new Negation(painted));
+
+            var r = q.Evaluate(world);
+            Assert.AreEqual(r, true);
+        }
+
+        [TestMethod]
+        public void EverAccessibleQuery()
+        {
+            world = CreateITWorld();
+            var q = new AccessibleEverQuery(new Negation(painted), new Negation(painted));
+
+            var r = q.Evaluate(world);
+            Assert.AreEqual(r, true);
+        }
+
+        [TestMethod]
+        public void AlwaysAccessibleQuery()
+        {
+            world = CreateITWorld();
+            var q = new AccessibleAlwaysQuery(new Negation(painted), new Negation(painted));
+
+            var r = q.Evaluate(world);
+            Assert.AreEqual(r, false);
+        }
+    }
+}
