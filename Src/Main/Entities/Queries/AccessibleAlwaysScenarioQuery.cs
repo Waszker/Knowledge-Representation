c53e9f7b3f3ae080649185c937eab6f77c4ec8ec
siryj <siryj@i.ua>
2016-06-13 21:40:23 +0200

    always matematycznie

--- Src/Main/Entities/Queries/AccessibleAlwaysScenarioQuery.cs
+++ Src/Main/Entities/Queries/AccessibleAlwaysScenarioQuery.cs
@@ -33,7 +33,7 @@ namespace KR.Main.Entities.Queries
                 states = states.SelectMany(s => world.GetEdges(s)
                 .Where(e => e.Action.Equals(step.Action) && e.Actor.Equals(step.Actor)).Select(e => e.To)).ToList();
             }
-            if (states.Count == 0) return false;
+            //if (states.Count == 0) return false;
             return states.All(s => gamma.Check(s));
         }
     }
---
+++ Src/Test/LastTests.cs
@@ -0,0 +1,31 @@
+﻿using KR.Main.Entities;
+using KR.Main.Entities.Conditions;
+using KR.Main.Entities.Statements;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KR.Test
+{
+    [TestClass]
+    public class LastTests
+    {
+        [TestMethod]
+        public void Load()
+        {
+            /*var domain = new Domain();
+            var a = new Fluent("a");
+            var l = new Fluent("l");
+            domain.AddInitiallyClause(new Initially(new Conjunction(a, l)));
+            var load = new Action("load");
+            var Bill = new Actor("Bill");
+            domain.AddCausesClause(new Causes(load, false, new List<Actor> { Bill }, l));
+            var shoot = new Action("shoot");
+            domain.AddCausesClause(new Causes(Ac))
+                    shoot Ac causes ~l
+            shoot Ac typically causes ~a if l*/
+        }
+    }
+}
