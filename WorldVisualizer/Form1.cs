e0319949785296b8899deff9f2b8b20b0a6e6a28
Piotr Waszkiewicz <Waszka23@gmail.com>
2016-06-17 18:59:09 +0200

    Added new test

--- WorldVisualizer/Form1.cs
+++ WorldVisualizer/Form1.cs
@@ -33,6 +33,13 @@ namespace WorldVisualizer
             DrawGraph();
         }
 
+        private Fluent alive = new Fluent("alive"),
+                       walking = new Fluent("walking"),
+                       loaded = new Fluent("loaded");
+        private Action shoot = new Action("SHOOT"),
+                       entice = new Action("ENTICE"),
+                       load = new Action("LOAD");
+
         protected World CreateITWorld()
         {
             var world = World.Instance;
@@ -43,17 +50,20 @@ namespace WorldVisualizer
             cc = new Fluent("cc");
             Fred = new Actor("Fred");
             Bill = new Actor("Bill");
-            world.SetActions(new List<Action>() { code, refactor, debug });
-            world.SetActors(new List<Actor>() { Fred, Bill });
-            world.SetFluents(new List<Fluent> { cc, cm });
+            world.SetActions(new List<Action>() { entice, shoot, load });
+            world.SetActors(new List<Actor>() { Bill });
+            world.SetFluents(new List<Fluent> { alive, walking, loaded });
             var domain = new Domain();
-            domain.AddCausesClause(new Causes(code, false, new List<Actor>() { Bill }, new Negation(cc)));
-            domain.AddInitiallyClause(new Initially(new Conjunction(cc, cm)));
-            domain.AddReleasesClause(new Releases(code, false, new List<Actor>() { Bill }, cm, cm));
-            domain.AddTypicallyCausesClause(new TypicallyCauses(code, false, new List<Actor>() { Fred }, cm, new Negation(cc)));
-            domain.AddCausesClause(new Causes(refactor, false, new List<Actor>() { Fred }, cc));
-            domain.AddCausesClause(new Causes(debug, true, null, cm));
-            domain.AddImpossibleClause(new Impossible(code, false, new List<Actor>() { Fred }, new Negation(cm)));
+
+            var emptyList = new List<Actor>();
+            domain.AddInitiallyClause(new Initially(new Conjunction(new Negation(loaded), alive)));
+            domain.AddCausesClause(new Causes(load, true, emptyList, loaded));
+            domain.AddCausesClause(new Causes(shoot, true, emptyList, new Negation(loaded)));
+            domain.AddCausesClause(new Causes(entice, true, emptyList, walking));
+            domain.AddTypicallyCausesClause(new TypicallyCauses(shoot, true, emptyList, new Negation(alive), loaded));
+            domain.AddAlwaysClause(new Always(new Implication(walking, alive)));
+            domain.AddPreservesClause(new Preserves(entice, true, emptyList, alive, null));
+
             world.SetDomain(domain);
             world.Build();
             return world;
